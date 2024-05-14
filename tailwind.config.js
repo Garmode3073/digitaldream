57709f7407f3b1b02309b8f1ece9a96b0222b8a4
LOGAN <gauravgarmode@gmail.com>
2024-05-15 00:59:51 +0530

    changes from git diff site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,13 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,tsx,jsx,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
-      colors:{
-        "black":"#000000",
-        "white":"#ffffff",
-        "titleFont":"#263238",
-        "grey900":"#717171",
-        "white800":"#cccccc",
-        "brandPrimary":"#003546",
-      },
-      margin: {
-        '15': '5rem',
-        '30': '30rem',
-      },
+      margin:{
+        "40":"100rem"
+      }
     },
   },
   plugins: [],
 }
-
